d0746599b54763c1b77bfb85ba4fffd870ebb22a
AndrewMarchukov <Jeklani@gmail.com>
2019-02-22 09:40:13 +0400

    Update tracker-add-auto.sh
    add feature connection to host

--- tracker-add-auto.sh
+++ tracker-add-auto.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
-# Get transmission credentials
+# Get transmission credentials and ip or dns ardress
 auth=user:password
+host=localhost
 while true ; do
 sleep 25
 add_trackers () {
@@ -11,7 +12,7 @@ for base_url in https://raw.githubusercontent.com/ngosang/trackerslist/master/tr
     echo "Adding trackers for $torrent_name..."
 for tracker in $(curl -# "${base_url}") ; do
     echo -n "${tracker}..."
-if transmission-remote  --auth="$auth" --torrent "${torrent_hash}" -td "${tracker}" | grep -q 'success'; then
+if transmission-remote "$host"  --auth="$auth" --torrent "${torrent_hash}" -td "${tracker}" | grep -q 'success'; then
     echo ' failed.'
 else
     echo ' done.'
@@ -22,17 +23,17 @@ done
     rm -f /tmp/TTAA.$id.lock
 }
 # Get list of active torrents
-    ids="$(transmission-remote --auth="$auth" --list | grep -vE 'Seeding|Stopped|Finished' | grep '^ ' | awk '{ print $1 }')"
+    ids="$(transmission-remote "$host" --auth="$auth" --list | grep -vE 'Seeding|Stopped|Finished' | grep '^ ' | awk '{ print $1 }')"
 for id in $ids ; do
-    add_date="$(transmission-remote --auth="$auth" --torrent "$id" --info| grep '^  Date added: ' |cut -c 21-)"
+    add_date="$(transmission-remote "$host" --auth="$auth" --torrent "$id" --info| grep '^  Date added: ' |cut -c 21-)"
     add_date_t="$(date -d "$add_date" "+%Y-%m-%d %H:%M")"
     dater="$(date "+%Y-%m-%d %H:%M")"
     dateo="$(date -d "1 minutes ago" "+%Y-%m-%d %H:%M")"
 
 if [ ! -f /tmp/TTAA.$id.lock ]; then
 if [[ ( "$add_date_t" == "$dater" || "$add_date_t" == "$dateo" ) ]]; then
-    hash="$(transmission-remote --auth="$auth" --torrent "$id" --info | grep '^  Hash: ' | awk '{ print $2 }')"
-    torrent_name="$(transmission-remote --auth="$auth" --torrent "$id" --info | grep '^  Name: ' |cut -c 9-)"
+    hash="$(transmission-remote "$host" --auth="$auth" --torrent "$id" --info | grep '^  Hash: ' | awk '{ print $2 }')"
+    torrent_name="$(transmission-remote "$host" --auth="$auth" --torrent "$id" --info | grep '^  Name: ' |cut -c 9-)"
     add_trackers "$hash" "$id" &
     touch /tmp/TTAA.$id.lock
 fi
