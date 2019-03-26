fd9d85461e48fdf544c0902c6e1bfb10ac932a0d
AndrewMarchukov <Jeklani@gmail.com>
2019-03-27 01:05:19 +0400

    Update tracker-add-auto.sh
    now tracker file saved in tmp directory until update
    new exception in "Get list of active torrents"

--- tracker-add-auto.sh
+++ tracker-add-auto.sh
@@ -9,9 +9,17 @@ add_trackers () {
     torrent_hash=$1
     id=$2
 for base_url in https://raw.githubusercontent.com/ngosang/trackerslist/master/trackers_all.txt ; do
+if [ ! -f /tmp/trackers.txt ]; then
+curl -o "/tmp/trackers.txt" "${base_url}"
+fi
+Local=$(wc -c < /tmp/trackers.txt)
+Remote=$(curl -sI "${base_url}" | awk '/Content-Length/ {sub("\r",""); print $2}')
+if [ $Local != $Remote ]; then
+curl -o "/tmp/trackers.txt" "${base_url}"
+fi
     echo "URL for ${base_url}"
     echo "Adding trackers for $torrent_name..."
-for tracker in $(curl -# "${base_url}") ; do
+for tracker in $(cat /tmp/trackers.txt) ; do
     echo -n "${tracker}..."
 if transmission-remote "$host"  --auth="$auth" --torrent "${torrent_hash}" -td "${tracker}" | grep -q 'success'; then
     echo ' failed.'
@@ -24,7 +32,7 @@ done
     rm -f /tmp/TTAA.$id.lock
 }
 # Get list of active torrents
-    ids="$(transmission-remote "$host" --auth="$auth" --list | grep -vE 'Seeding|Stopped|Finished' | grep '^ ' | awk '{ print $1 }')"
+    ids="$(transmission-remote "$host" --auth="$auth" --list | grep -vE 'Seeding|Stopped|Finished|[[:space:]]100%[[:space:]]' | grep '^ ' | awk '{ print $1 }')"
 for id in $ids ; do
     add_date="$(transmission-remote "$host" --auth="$auth" --torrent "$id" --info| grep '^  Date added: ' |cut -c 21-)"
     add_date_t="$(date -d "$add_date" "+%Y-%m-%d %H:%M")"
