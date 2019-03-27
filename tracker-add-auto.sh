0691f9ec0399b8e67fe9acb02bd54f0c4f42f72d
AndrewMarchukov <Jeklani@gmail.com>
2019-03-27 13:05:38 +0400

    Update tracker-add-auto.sh
    cosmetic fixes

--- tracker-add-auto.sh
+++ tracker-add-auto.sh
@@ -8,18 +8,19 @@ sleep 25
 add_trackers () {
     torrent_hash=$1
     id=$2
+    trackerslist=/tmp/trackers.txt
 for base_url in https://raw.githubusercontent.com/ngosang/trackerslist/master/trackers_all.txt ; do
-if [ ! -f /tmp/trackers.txt ]; then
-curl -o "/tmp/trackers.txt" "${base_url}"
+if [ ! -f $trackerslist ]; then
+curl -o "$trackerslist" "${base_url}"
 fi
-Local=$(wc -c < /tmp/trackers.txt)
+Local=$(wc -c < $trackerslist)
 Remote=$(curl -sI "${base_url}" | awk '/Content-Length/ {sub("\r",""); print $2}')
-if [ $Local != $Remote ]; then
-curl -o "/tmp/trackers.txt" "${base_url}"
+if [ "$Local" != "$Remote" ]; then
+curl -o "$trackerslist" "${base_url}"
 fi
     echo "URL for ${base_url}"
     echo "Adding trackers for $torrent_name..."
-for tracker in $(cat /tmp/trackers.txt) ; do
+for tracker in $(cat $trackerslist) ; do
     echo -n "${tracker}..."
 if transmission-remote "$host"  --auth="$auth" --torrent "${torrent_hash}" -td "${tracker}" | grep -q 'success'; then
     echo ' failed.'
@@ -29,7 +30,7 @@ fi
 done
 done
     sleep 3m
-    rm -f /tmp/TTAA.$id.lock
+    rm -f "/tmp/TTAA.$id.lock"
 }
 # Get list of active torrents
     ids="$(transmission-remote "$host" --auth="$auth" --list | grep -vE 'Seeding|Stopped|Finished|[[:space:]]100%[[:space:]]' | grep '^ ' | awk '{ print $1 }')"
@@ -39,12 +40,12 @@ for id in $ids ; do
     dater="$(date "+%Y-%m-%d %H:%M")"
     dateo="$(date -d "1 minutes ago" "+%Y-%m-%d %H:%M")"
 
-if [ ! -f /tmp/TTAA.$id.lock ]; then
-if [[ ( "$add_date_t" == "$dater" || "$add_date_t" == "$dateo" ) ]]; then
+if [ ! -f "/tmp/TTAA.$id.lock" ]; then
+if [[ "( "$add_date_t" == "$dater" || "$add_date_t" == "$dateo" )" ]]; then
     hash="$(transmission-remote "$host" --auth="$auth" --torrent "$id" --info | grep '^  Hash: ' | awk '{ print $2 }')"
     torrent_name="$(transmission-remote "$host" --auth="$auth" --torrent "$id" --info | grep '^  Name: ' |cut -c 9-)"
     add_trackers "$hash" "$id" &
-    touch /tmp/TTAA.$id.lock
+    touch "/tmp/TTAA.$id.lock"
 fi
 fi
 done
